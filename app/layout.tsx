1c019c58871aff732e838c91c2e102b663adf722
Maksym Rudnyi <max.prog.zz@gmail.com>
2024-07-25 22:47:53 +0300

    styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,26 @@
+import './globals.css'
+import {Providers} from "./providers";
+import { Inter } from 'next/font/google'
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={inter.className}>
+      <body>
+        <Providers>
+          <div className="flex min-h-screen flex-col overflow-hidden supports-[overflow:clip]:overflow-clip">
+            {children}
+          </div>
+        </Providers>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import {Button} from "@nextui-org/button";
+
 export const metadata = {
   title: "App Router",
 };
 
 export default function Page() {
-  return <h1>App Router</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+      <Button color={'primary'}>click</Button>
+  </h1>
+  )
 }
