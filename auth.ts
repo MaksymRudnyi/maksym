285db00a6bbabfe3de1d98b8da2307d7dd174fa6
Maksym Rudnyi <max.prog.zz@gmail.com>
2024-07-30 20:24:54 +0300

    try auth with Auth0: Prisma + MongoDB

---
+++ auth.ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client/edge";
+
+const prisma = new PrismaClient();
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [GitHub],
+  debug: true,
+});
