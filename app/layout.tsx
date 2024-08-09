742eab8edcf17326006c75d2d6c432633af72ec9
Maksym Rudnyi <max.prog.zz@gmail.com>
2024-08-09 14:35:30 +0300

    add Google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { auth } from "@/auth";
 import { Footer, Navigation } from "@/components";
 import { UserProfile } from "@/types/UserProfile";
+import { GoogleTagManager } from "@next/third-parties/google";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 
@@ -24,6 +25,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en" className={inter.className}>
+      <GoogleTagManager gtmId="GTM-NQKJN7JH" />
       <body>
         <Providers>
           <div className="flex min-h-screen flex-col">
